d7cfe6bb8390e6fc352dbfe6c5d2825c65a09de8
shubham1020k <gyaninfo128@gmail.com>
2022-06-09 09:59:03 +0530

    activity_main.xml AndroidManifest.xml AppModule build.gradle MainActivity MoviesAdapter MoviesLoadStateAdapter

--- app/src/main/java/com/mastercoding/pagingapp/activity/MainActivity.java
+++ app/src/main/java/com/mastercoding/pagingapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mastercoding.pagingapp;
+package com.mastercoding.pagingapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mastercoding.pagingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
