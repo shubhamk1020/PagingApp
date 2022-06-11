6a35ce9e42af9a66596e6cf91d98686b3f72b243
shubham1020k <gyaninfo128@gmail.com>
2022-06-11 08:11:20 +0530

    build.gradle MainActivity MoviesApp MovieViewModel settings.gradle gradle.properties

--- app/src/main/java/com/mastercoding/pagingapp/util/GridSpace.java
+++ app/src/main/java/com/mastercoding/pagingapp/util/GridSpace.java
@@ -20,23 +20,22 @@ public class GridSpace extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-          int position = parent.getChildAdapterPosition(view);
-          int column = position % spanCount;//item Column
-        if(includeEdge){
-            outRect.left = spacing - column * spacing/spanCount;
-            outRect.right = (column + 1) * spacing/spanCount;
+        int position = parent.getChildAdapterPosition(view); // item position
+        int column = position % spanCount; // item column
 
-            if(position < spanCount){   //top edge
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+            outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+            if (position < spanCount) { // top edge
                 outRect.top = spacing;
-             }
-            outRect.bottom = spacing;     //item bottom
-
-        }
-        else {
-            outRect.left = column * spacing/spanCount;
-            outRect.right = spacing - (column + 1) * spacing/spanCount;
-            if(position >= spanCount){
-                outRect.top = spacing;     // item top
+            }
+            outRect.bottom = spacing; // item bottom
+        } else {
+            outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+            outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+            if (position >= spanCount) {
+                outRect.top = spacing; // item top
             }
         }
     }
--- app/src/main/java/com/mastercoding/pagingapp/util/Utils.java
+++ app/src/main/java/com/mastercoding/pagingapp/util/Utils.java
@@ -3,5 +3,5 @@ package com.mastercoding.pagingapp.util;
 public class Utils {
 
     public static String API_KEY = "79faaaa4592ef5d4279c97d15316ccb0";
-            public static String BASE_URL = "https://api.themoviedb.org/3";
+            public static String BASE_URL = "https://api.themoviedb.org/3/";
 }
